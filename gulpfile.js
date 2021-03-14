498eb78487f629525970ecde95348c6777096701
Konrad Hartmann <konradhartmannv@hotmail.com>
2021-03-14 13:17:05 -0300

    gulp np pup

--- gulpfile.js
+++ gulpfile.js
@@ -1,77 +1,50 @@
 // Themesberg
-var gulp        = require('gulp');
-var browserSync = require('browser-sync').create();
-var sass        = require('gulp-sass');
+const gulp          = require('gulp');
+const browserSync   = require('browser-sync').create();
+const sass          = require('gulp-sass');
+// const pug           = require('gulp-pug');
+const postcss       = require('gulp-postcss');
+const autoprefixer  = require('autoprefixer');
+// const prettier      = require('gulp-prettier');
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
-    return gulp.src("app/scss/*.scss")
+    return gulp.src("src/scss/*.scss")
         .pipe(sass())
-        .pipe(gulp.dest("app/css"))
+        .pipe(postcss([autoprefixer()]))
+        .pipe(gulp.dest("dist/css"))
         .pipe(browserSync.stream());
 });
+
+// // pug is not ugly
+// gulp.task('pug', function() {
+//     return gulp.src('src/pug/*.pug')
+//      .pipe(pug({ locals: {}, pretty: true }))
+//      .pipe(prettier({
+//         printWidth: 1000,
+//         tabWidth: 4,
+//         singleQuote: true,
+//         proseWrap: 'preserve',
+//         endOfLine: 'lf',
+//         parser: 'html',
+//         htmlWhitespaceSensitivity: 'ignore'
+//     }))
+//      .pipe(gulp.dest('./'))
+//      .pipe(browserSync.stream());
+//   })
 
 // Static Server + watching scss/html files
 gulp.task('serve', gulp.series('sass', function() {
 
     browserSync.init({
-        server: "./app/"
+        server: "./"
     });
 
-    gulp.watch("app/scss/*.scss", gulp.series('sass'));
+    gulp.watch("src/scss/*.scss", gulp.series('sass'));
+    // gulp.watch("src/pug/*.pug", gulp.series('pug'));
     gulp.watch([ "app/*.html", "app/js/*.js"]).on('change', browserSync.reload);
     
 }));
 
 gulp.task('default', gulp.series('serve'));
     
-// const { src, dest, watch, series } = require('gulp');
-// const sass = require('gulp-sass');
-// const postcss = require('gulp-postcss');
-// const cssnano = require('cssnano');
-// const terser = require('gulp-terser');
-// const browsersync = require('browser-sync').create();
-
-// // Sass Task
-// function scssTask(){
-//   return src('app/scss/style.scss', { sourcemaps: true })
-//     .pipe(sass())
-//     .pipe(postcss([cssnano()]))
-//     .pipe(dest('dist', { sourcemaps: '.' }));
-// }
-
-// // JavaScript Task
-// function jsTask(){
-//   return src('app/js/script.js', { sourcemaps: true })
-//     .pipe(terser())
-//     .pipe(dest('dist', { sourcemaps: '.' }));
-// }
-
-// // Browsersync Tasks
-// function browsersyncServe(cb){
-//   browsersync.init({
-//     server: {
-//       baseDir: '.'
-//     }
-//   });
-//   cb();
-// }
-
-// function browsersyncReload(cb){
-//   browsersync.reload();
-//   cb();
-// }
-
-// // Watch Task
-// function watchTask(){
-//   watch('*.html', browsersyncReload);
-//   watch(['app/scss/**/*.scss', 'app/js/**/*.js'], series(scssTask, jsTask, browsersyncReload));
-// }
-
-// // Default Gulp task
-// exports.default = series(
-//   scssTask,
-//   jsTask,
-//   browsersyncServe,
-//   watchTask
-// );
