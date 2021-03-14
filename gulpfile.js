07c3f1f2a3bca39c6e31ba72b1a13aaad9165591
Konrad Hartmann <konradhartmannv@hotmail.com>
2021-03-14 12:37:35 -0300

    added SCSS for main page

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,30 @@
+// Themesberg
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src("app/scss/*.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("app/css"))
+        .pipe(browserSync.stream());
+});
+
+// Static Server + watching scss/html files
+gulp.task('serve', gulp.series('sass', function() {
+
+    browserSync.init({
+        server: "./app/"
+    });
+
+    gulp.watch("app/scss/*.scss", gulp.series('sass'));
+    gulp.watch([ "app/*.html", "app/js/*.js"]).on('change', browserSync.reload);
+    
+}));
+
+gulp.task('default', gulp.series('serve'));
+    
 // const { src, dest, watch, series } = require('gulp');
 // const sass = require('gulp-sass');
 // const postcss = require('gulp-postcss');
@@ -48,31 +75,3 @@
 //   browsersyncServe,
 //   watchTask
 // );
-
-// Themesberg
-var gulp        = require('gulp');
-var browserSync = require('browser-sync').create();
-var sass        = require('gulp-sass');
-
-// Compile sass into CSS & auto-inject into browsers
-gulp.task('sass', function() {
-    return gulp.src("app/scss/*.scss")
-        .pipe(sass())
-        .pipe(gulp.dest("app/css"))
-        .pipe(browserSync.stream());
-});
-
-// Static Server + watching scss/html files
-gulp.task('serve', gulp.series('sass', function() {
-
-    browserSync.init({
-        server: "./app/"
-    });
-
-    gulp.watch("app/scss/*.scss", gulp.series('sass'));
-    gulp.watch([ "app/*.html", "app/js/*.js"]).on('change', browserSync.reload);
-    
-}));
-
-gulp.task('default', gulp.series('serve'));
-    
