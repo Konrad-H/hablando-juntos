f72b64ad75bb1d59c9592306e5cc7391bfdc11b4
Konrad Hartmann <konradhartmannv@hotmail.com>
2021-03-14 14:24:42 -0300

    added sass workflow for browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,15 @@ const gulp          = require('gulp');
 const browserSync   = require('browser-sync').create();
 const sass          = require('gulp-sass');
 // const pug           = require('gulp-pug');
-const postcss       = require('gulp-postcss');
-const autoprefixer  = require('autoprefixer');
+// const postcss       = require('gulp-postcss');
+// const autoprefixer  = require('autoprefixer');
 // const prettier      = require('gulp-prettier');
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
-    return gulp.src("src/scss/*.scss")
+    return gulp.src("app/scss/*.scss")
         .pipe(sass())
-        .pipe(postcss([autoprefixer()]))
-        .pipe(gulp.dest("dist/css"))
+        .pipe(gulp.dest("app/css"))
         .pipe(browserSync.stream());
 });
 
